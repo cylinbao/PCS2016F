7ab5289e70687e318423365d5264868ac9c5fc25
Chien Yu Lin <myislin@gmail.com>
2016-10-27 22:26:55 +0800

    source code for lab0

---
+++ Lab0/obj/hello_ucosii.d
@@ -0,0 +1,41 @@
+obj/hello_ucosii.o: hello_ucosii.c ../Lab0_bsp//HAL/inc/includes.h \
+  ../Lab0_bsp//HAL/inc/os_cpu.h ../Lab0_bsp//HAL/inc/sys/alt_irq.h \
+  ../Lab0_bsp//HAL/inc/nios2.h ../Lab0_bsp//HAL/inc/alt_types.h \
+  ../Lab0_bsp/system.h ../Lab0_bsp/linker.h \
+  ../Lab0_bsp//UCOSII/inc/os_cfg.h ../Lab0_bsp//HAL/inc/sys/alt_alarm.h \
+  ../Lab0_bsp//HAL/inc/sys/alt_llist.h \
+  ../Lab0_bsp//HAL/inc/priv/alt_alarm.h ../Lab0_bsp//HAL/inc/alt_types.h \
+  ../Lab0_bsp/system.h ../Lab0_bsp//UCOSII/inc/ucos_ii.h \
+  ../Lab0_bsp//UCOSII/inc/os_cfg.h ../Lab0_bsp//HAL/inc/os_cpu.h
+
+../Lab0_bsp//HAL/inc/includes.h:
+
+../Lab0_bsp//HAL/inc/os_cpu.h:
+
+../Lab0_bsp//HAL/inc/sys/alt_irq.h:
+
+../Lab0_bsp//HAL/inc/nios2.h:
+
+../Lab0_bsp//HAL/inc/alt_types.h:
+
+../Lab0_bsp/system.h:
+
+../Lab0_bsp/linker.h:
+
+../Lab0_bsp//UCOSII/inc/os_cfg.h:
+
+../Lab0_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Lab0_bsp//HAL/inc/sys/alt_llist.h:
+
+../Lab0_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Lab0_bsp//HAL/inc/alt_types.h:
+
+../Lab0_bsp/system.h:
+
+../Lab0_bsp//UCOSII/inc/ucos_ii.h:
+
+../Lab0_bsp//UCOSII/inc/os_cfg.h:
+
+../Lab0_bsp//HAL/inc/os_cpu.h:
